a8461ac1037cd07e800051a52dcdf06322fbdd54
Ramon2112 <romualdo.mangahasjr@gmail.com>
2026-05-31 01:15:45 +0800

    Ensure index.css is imported in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import './index.css'
+import './index.css' // <-- MAKE SURE THIS LINE IS HERE!
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
